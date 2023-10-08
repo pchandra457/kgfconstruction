85dc871ecb094d17ab898a9fdb0866057b09d950
pchandra457 <pchandra457@gmail.com>
2023-10-08 20:06:36 +0530

    Create kng-assoc.php

---
+++ kng-assoc.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Basic Page Needs
+================================================== -->
+    <meta charset="utf-8" />
+    <title>KGF Construction</title>
+
+    <!-- Mobile Specific Metas
+================================================== -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="Plan better build better with our honesty & dedication." />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, maximum-scale=5.0"
+    />
+
+    <!-- Favicon
+================================================== -->
+    <link rel="icon" type="image/png" href="images/favicon.jpeg" />
+
+    <!-- CSS
+================================================== -->
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css" />
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="plugins/fontawesome/css/all.min.css" />
+    <!-- Animation -->
+    <link rel="stylesheet" href="plugins/animate-css/animate.css" />
+    <!-- slick Carousel -->
+    <link rel="stylesheet" href="plugins/slick/slick.css" />
+    <link rel="stylesheet" href="plugins/slick/slick-theme.css" />
+    <!-- Colorbox -->
+    <link rel="stylesheet" href="plugins/colorbox/colorbox.css" />
+    <!-- Template styles-->
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <div class="body-inner">
+      <!-- Header start -->
+      <?php include 'header.php';?>
+      <!--/ Header end -->
+      <div
+        id="banner-area"
+        class="banner-area"
+        style="background-image: url(images/banner/main-banner.png)"
+      >
+        <div class="banner-text">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="banner-heading">
+                  <h1 class="banner-title">Service</h1>
+                  <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb justify-content-center">
+                      <li class="breadcrumb-item"><a href="#">Home</a></li>
+                      <li class="breadcrumb-item active" aria-current="page">
+                        All Services
+                      </li>
+                    </ol>
+                  </nav>
+                </div>
+              </div>
+              <!-- Col end -->
+            </div>
+            <!-- Row end -->
+          </div>
+          <!-- Container end -->
+        </div>
+        <!-- Banner text end -->
+      </div>
+      <!-- Banner area end -->
+
+      <section id="main-container" class="main-container">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-xl-3 col-lg-4">
+        <div class="sidebar sidebar-left">
+          <div class="widget">
+            <h3 class="widget-title">Services</h3>
+            <ul class="nav service-menu">
+              <li><a href="#">Residential</a></li>
+              <li><a href="#">Apartment</a></li>
+              <li><a href="#">Commercial Complex</a></li>
+              <li><a href="#">Hospital</a></li>
+              <li><a href="#">Hotel</a></li>
+            </ul>
+          </div><!-- Widget end -->
+
+        </div><!-- Sidebar end -->
+      </div><!-- Sidebar Col end -->
+
+      <div class="col-xl-8 col-lg-8">
+        <div class="content-inner-page">
+
+          <h2 class="column-title mrt-0">K&G Associate</h2>
+
+          <div class="row">
+            <div class="col-md-12">
+              <p>K&G Associate, a team of best architects and interior designer, you can rely on us for your Home design, Approval of Municipal Drawings, Plans. We provide all architectural service for residential and commercial building.</p>
+            </div><!-- col end -->
+          </div><!-- 1st row end-->
+
+          <div class="gap-40"></div>
+
+          <div id="page-slider" class="page-slider">
+            <div class="item">
+              <img loading="lazy" class="img-fluid" src="https://kgfconstruction.com/images/projects/project1.jpg" alt="project-slider-image" />
+            </div>
+
+            <div class="item">
+              <img loading="lazy" class="img-fluid" src="https://kgfconstruction.com/images/projects/project6.jpg" alt="project-slider-image" />
+            </div>
+          </div><!-- Page slider end -->
+
+          <div class="gap-40"></div>
+
+          <div class="row">
+            <div class="col-md-6">
+              <h3 class="column-title-small">What Makes Us Different</h3>
+
+              <p>All our projects are conceived and executed under the direct supervision of architect. Our highly competent and skilled workforce uses the highest quality materials to create aesthetic, contemporary, innovative and luxurious interiors and exteriors.</p>
+              <ul class="list-arrow">
+                <li>Partnership Strategy tristique eleifend.</li>
+                <li>Opporutnity to work with amet elit a.</li>
+                <li>Saving Time to Deal with commodo iaculis.</li>
+                <li>Leadership skills to manage erat volutpat.</li>
+                <li>Cut cost without sacrificing dolore magna.</li>
+                <li>Automate your business elis tristique.</li>
+              </ul>
+            </div>
+
+            <div class="col-md-6 mt-5 mt-md-0">
+              <h3 class="column-title-small">You Should Know</h3>
+
+              <div class="accordion accordion-group accordion-classic" id="construction-accordion">
+                <div class="card">
+                  <div class="card-header p-0 bg-transparent" id="headingOne">
+                    <h2 class="mb-0">
+                      <button class="btn btn-block text-left" type="button" data-toggle="collapse"
+                        data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Does the law require head protection on construction sites?
+                      </button>
+                    </h2>
+                  </div>
+
+                  <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                    data-parent="#construction-accordion">
+                    <div class="card-body">
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
+                      wolf
+                      moon officia aute, non cupidata
+                    </div>
+                  </div>
+                </div>
+                <div class="card">
+                  <div class="card-header p-0 bg-transparent" id="headingTwo">
+                    <h2 class="mb-0">
+                      <button class="btn btn-block text-left collapsed" type="button" data-toggle="collapse"
+                        data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        What are the first aid requirements for sites?
+                      </button>
+                    </h2>
+                  </div>
+                  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo"
+                    data-parent="#construction-accordion">
+                    <div class="card-body">
+                      Anemi nim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea com modo
+                      consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                      pariatur.
+                      Excepteur sint occaecat cupid henderit in voluptate velit esse cillu oris nisi ut aliquip ex ea
+                      com
+                      matat.
+                    </div>
+                  </div>
+                </div>
+                <div class="card">
+                  <div class="card-header p-0 bg-transparent" id="headingThree">
+                    <h2 class="mb-0">
+                      <button class="btn btn-block text-left collapsed" type="button" data-toggle="collapse"
+                        data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        What is an appointed person?
+                      </button>
+                    </h2>
+                  </div>
+                  <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
+                    data-parent="#construction-accordion">
+                    <div class="card-body">
+                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                      the
+                      industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                      and
+                      scrambled it to make a type specimen book.
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!--/ Accordion end -->
+            </div>
+          </div>
+          <!--2nd row end -->
+
+          <div class="gap-40"></div>
+
+        </div><!-- Content inner end -->
+      </div><!-- Content Col end -->
+
+
+    </div><!-- Main row end -->
+  </div><!-- Conatiner end -->
+</section><!-- Main container end -->
+      <!-- Main container end -->
+
+      <!-- Footer start -->
+      <?php include 'footer.php';?>
+      <!-- Footer end -->
+
+      <!-- Javascript Files
+  ================================================== -->
+
+      <!-- initialize jQuery Library -->
+      <script src="plugins/jQuery/jquery.min.js"></script>
+      <!-- Bootstrap jQuery -->
+      <script src="plugins/bootstrap/bootstrap.min.js" defer></script>
+      <!-- Slick Carousel -->
+      <script src="plugins/slick/slick.min.js"></script>
+      <script src="plugins/slick/slick-animation.min.js"></script>
+      <!-- Color box -->
+      <script src="plugins/colorbox/jquery.colorbox.js"></script>
+      <!-- shuffle -->
+      <script src="plugins/shuffle/shuffle.min.js" defer></script>
+
+      <!-- Google Map Plugin-->
+      <script src="plugins/google-map/map.js" defer></script>
+
+      <!-- Template custom -->
+      <script src="js/script.js"></script>
+    </div>
+    <!-- Body inner end -->
+  </body>
+</html>
